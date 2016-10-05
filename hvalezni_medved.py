bcb3dc28cc68d63ba85c813beb1a194740089260
Luka <luka.null@gmail.com>
2016-10-05 19:54:11 +0200

    Prvi commit prvih vaj

---
+++ hvalezni_medved.py
@@ -0,0 +1,117 @@
+
+import re
+##########################################################################
+# Hvaležni medved
+#
+# Pri tej nalogi bomo napisali nekaj funkcij, ki nam bodo v pomoč pri analizi
+# literarnih besedil, kot je na primer koroška narodna pripovedka *Hvaležni
+# medved*.
+#
+# Grateful Bear
+#
+# In this exercise we will write a few functions that help us analyse literary
+# texts, such as the Carinthian folk tale "Grateful Bear".
+#
+##########################################################################
+
+odlomek = """Gori nekje v gorah, ne ve se več, ali je bilo pri Macigoju ali
+Naravniku, je šivala gospodinja v senci pod drevesom in zibala otroka. Naenkrat
+prilomasti - pa prej ni ničesar opazila - medved in ji moli taco, v kateri je
+tičal velik, debel trn. Žena se je prestrašila, a medved le milo in pohlevno
+godrnja. Zato se žena ojunači in mu izdere trn iz tace. Mrcina kosmata pa zvrne
+zibel, jo pobaše in oddide. Čez nekaj časa pa ji zopet prinese zibel, a zvhano
+napolnjeno s sladkimi hruškami . Postavil jo je na tla pred začudeno mater in
+odracal nazaj v goščavo. "Poglej no", se je razveselila mati, "kakšen hvaležen
+medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
+
+##########################################################################
+# 1) Sestavite funkcijo najdi_besede(besedilo, podniz), ki vrne množico
+# vseh besed, ki se pojavijo v nizu besedilo in vsebujejo niz podniz.
+# Zgled:
+#
+# >>> najdi_besede(odlomek, 'de')
+# {'izdere', 'debel', 'oddide', 'začudeno'}
+#
+# 1) Write a function find_words(text, substring) that returns a set of all the
+#    words in the text containing substring as substring.
+#
+# Example:
+# >>> find_words(odlomek, 'de')
+# {'izdere', 'debel', 'oddide', 'začudeno'}
+##########################################################################
+
+
+def najdi_besede(besedilo, podniz):
+    p = re.compile(r'\b(\w*{}\w*)\b'.format(podniz))
+    t = p.findall(besedilo)
+    return set(t)
+
+print(najdi_besede(odlomek, 'de'))
+
+##########################################################################
+# 2) Sestavite funkcijo najdi_predpono(besedilo, predpona), ki vrne množico
+# vseh besed, ki se pojavijo v nizu besedilo in imajo predpono predpona.
+# Zgled:
+#
+# >>> najdi_predpono(odlomek, 'zi')
+# {'zibala', 'zibel', 'zibelko'}
+#
+# 2) Write a function find_prefix(text, prefix) which returns the set of all
+#    words in the text starting with prefix.
+#
+# Example:
+# >>> find_prefix(odlomek, 'zi')
+# {'zibala', 'zibel', 'zibelko'}
+##########################################################################
+
+def najdi_predpono(besedilo, predpona):
+    p = re.compile(r'\b({}\w*)\b'.format(predpona))
+    t = p.findall(besedilo)
+    return set(t)
+
+print(najdi_predpono(odlomek, 'zi'))
+
+##########################################################################
+# 3) Sestavite funkcijo najdi_pripono(besedilo, pripona), ki vrne množico
+# vseh besed, ki se pojavijo v nizu besedilo in imajo pripono pripona.
+# Zgled:
+#
+# >>> najdi_pripono(odlomek, 'la')
+# {'zibala', 'razveselila', 'prestrašila', 'šivala', 'opazila', 'tla'}
+#
+# 3) Write a function find_suffix(text, suffix) which returns the set of all
+#    words in the text ending with suffix.
+#
+# Example:
+# >>> find_suffix(odlomek, 'la')
+# {'zibala', 'razveselila', 'prestrašila', 'šivala', 'opazila', 'tla'}
+##########################################################################
+
+def najdi_pripono(besedilo, predpona):
+    p = re.compile(r'(\w*{})\b'.format(predpona))
+    t = p.findall(besedilo)
+    return set(t)
+
+print(najdi_pripono(odlomek, 'la'))
+
+##########################################################################
+# 4) Sestavite funkcijo podvojene_crke(besedilo), ki sprejme niz besedilo
+# in vrne množico vseh besed, ki vsebujejo podvojene črke. Zgled:
+#
+# >>> podvojene_crke('A volunteer is worth twenty pressed men.')
+# {'pressed', 'volunteer'}
+#
+# 4) Write a function double_letters(text) that returns the set of words in
+#    text that contain the same letter twice consecutively.
+#
+# Example:
+# >>> double_letters('A volunteer is worth twenty pressed men.')
+# {'volunteer', 'pressed'}
+##########################################################################
+
+def podvojene_crke(besedilo):
+    p = re.compile(r'\b(\w*(\w)\2\w*)\b')
+    t = p.findall(besedilo)
+    return [a[0] for a in t]
+
+print(podvojene_crke('A volunteer is worth twenty pressed men.'))
---
+++ muce_z_bolhe.py
@@ -0,0 +1,76 @@
+import re
+import requests
+import csv
+import time
+
+'''
+    File name: muce_z_bolhe.py
+    Author: Luka Lodrant
+    Date created: 5/10/2016
+    Python Version: 3.5
+'''
+
+def get_sites():
+    site = requests.get("http://www.bolha.com/zivali/male-zivali/macke/?listingType=list&page=1").text
+
+    number_regex = re.compile(r'Št. najdenih oglasov: (\d*)', re.MULTILINE)
+
+    number_sites = (int(number_regex.findall(site)[0]) // 20) - 18
+
+    for i in range(2, number_sites):
+        print(number_sites - i)
+        site += requests.get("http://www.bolha.com/zivali/male-zivali/macke/?listingType=list&page=" + str(i)).text
+    return site;
+
+
+def get_ads(sites):
+    p = re.compile(
+        r'<div class="ad">.*?<a title="(.+?)".*?</h3>(.+?)<div.*?<div class="price">(?:<span>)?(.*?)(?:</span>)?</div>',
+        re.MULTILINE | re.DOTALL)
+    cats = p.findall(sites)
+    for c in cats:
+        print(c[1])
+    return [{"Naslov": c[0].strip(), "Opis": c[1].strip(), "Cena": c[2].strip()} for c in cats]
+
+
+def write_csv(filename, ads):
+    with open('muce.csv', 'w', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, dialect='excel', fieldnames=["Naslov", "Opis", "Cena"], )
+        writer.writeheader()
+        writer.writerows(ads)
+
+
+def send_file(filename, address, subject):
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = "luka.null@gmail.com"
+    msg['To'] = address
+
+    with open(filename, encoding='utf-8') as fp:
+        attachment = MIMEText(fp.read(), "plain")
+        attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+        msg.attach(attachment)
+
+    #Gmail
+    gmail_user = 'luka.null@gmail.com'
+    gmail_pwd = 'not telling'
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(gmail_user, gmail_pwd)
+    smtpserver.send_message(msg)
+    smtpserver.quit()
+
+def runner():
+    ads = get_ads(get_sites())
+    write_csv('muce.csv', ads)
+    send_file('muce.csv', "luka.lodrant@gmail.com", "Trenutna ponudba muc")
+    time.sleep(60*60)
+    runner()
+
+runner()
